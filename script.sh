6f0a64dede5fd37550c5d8426cc0505fa409be61
VikaThorn <v.yu.shipilova@gmail.com>
2024-11-18 17:49:29 +0300

    Lab 1. Task 15.

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+# Task 15.
+for dir in /sbin /usr/sbin; do   
+ for file in "$dir"/*; do
+  if [ -x "$file" ] && [ -f "$file" ]; then           
+   echo "$file" >> executable_files.txt
+  fi   
+ done
+done
